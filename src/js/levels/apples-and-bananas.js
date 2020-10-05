c50ada474d65bef5f6e963dc8afb2441b5389f04
Jacek Kopecky <jacek@jacek.cz>
2020-10-05 20:43:12 +0100

    add more levels

--- src/js/levels/apples-and-bananas.js
+++ src/js/levels/apples-and-bananas.js
@@ -140,12 +140,54 @@ function applesAndBananas3(rng) {
   return applesAndBananas(rng, x1, x2, y1, y2, a, b);
 }
 
-// function applesAndBananas4(rng) {
-//   // Like applesAndBananas3, but:
-//   //
-//   // - x1 not divisible by x2; x1 and x2 have a low lowest common multiple (LCM) L
-//   // - y1*L/x1 > y2*L/x2
-// }
+function applesAndBananas4(rng) {
+  // Like 2 but
+  //
+  // - x1 not divisible by x2; x1 and x2 have a low multiple M
+  // - y1*M/x1 > y2*M/x2
+
+  const x2 = rng.int(2, 4);
+  const x1 = nthNonMultiple(x2, rng.int(1, 3));
+  const y2 = rng.int(1, 3);
+  const y1 = Math.floor(y2 / x2 * x1) + rng.int(1, 3);
+  const a = rng.int(1, 11) * 0.1;
+  const b = rng.int(1, 11) * 0.1;
+
+  return applesAndBananas(rng, x1, x2, y1, y2, a, b);
+}
+
+// return nth number in the sequence x+1, x+2... excluding multiples of x
+function nthNonMultiple(x, n) {
+  const generator = nonMultiples(x);
+  for (const nonMultiple of generator) {
+    if (n === 1) return nonMultiple;
+    n -= 1;
+  }
+  return null; // this line will never be reached
+}
+
+function* nonMultiples(start) {
+  if (start < 2 || !Number.isInteger(start)) throw new TypeError('nonMultiples only works with integers > 1');
+  let x = start + 1;
+  while (true) {
+    if (x % start !== 0) yield x;
+    x += 1;
+  }
+}
+
+// 5: where subtracting one from the other leads to a negative?
+function applesAndBananas5(rng) {
+  // simply x1 > x2, y1 < y2, with prices in 5p increments
+
+  const x2 = rng.int(1, 3);
+  const x1 = x2 + rng.int(1, 3);
+  const y1 = rng.int(1, 3);
+  const y2 = y1 + rng.int(1, 3);
+  const a = rng.int(1, 22) * 0.05;
+  const b = rng.int(1, 22) * 0.05;
+
+  return applesAndBananas(rng, x1, x2, y1, y2, a, b);
+}
 
 export default [
   applesAndBananas1,
@@ -154,4 +196,8 @@ export default [
   onlyText(applesAndBananas2),
   applesAndBananas3,
   onlyText(applesAndBananas3),
+  applesAndBananas4,
+  onlyText(applesAndBananas4),
+  applesAndBananas5,
+  onlyText(applesAndBananas5),
 ];
