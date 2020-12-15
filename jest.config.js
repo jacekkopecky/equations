2c296326a0ecfb221a75770ca93172a87ae115d2
Jacek Kopecky <jacek@jacek.cz>
2020-12-15 12:14:13 +0000

    add typescript machinery

--- jest.config.js
+++ jest.config.js
@@ -92,7 +92,9 @@ module.exports = {
 
   // An array of regexp pattern strings, matched against all module paths before considered
   // 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: [
+    '/dist-ts/',
+  ],
 
   // Activates notifications for test results
   // notify: false,
