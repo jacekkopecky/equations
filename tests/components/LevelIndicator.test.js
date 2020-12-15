2efec7a9ace1f9d0335b9990838d84d1f2fb5055
Jacek Kopecky <jacek@jacek.cz>
2020-12-15 16:40:38 +0000

    add a simple LevelIndicator test

--- tests/components/LevelIndicator.test.js
+++ tests/components/LevelIndicator.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, shallow } from 'enzyme';
 
 import LevelIndicator from '../../src/js/components/LevelIndicator';
 
@@ -7,4 +7,11 @@ import * as states from './testing-app-states';
 
 test('renders without exception', () => {
   expect(() => render(<LevelIndicator appState={states.empty()} />)).not.toThrow();
+});
+
+test('shows five stars', () => {
+  const appState = states.empty();
+  const wrapper = shallow(<LevelIndicator appState={appState} />);
+  expect(wrapper.find('.star')).toHaveLength(appState.progressRequired);
+  expect(appState.progressRequired).toBe(5);
 });
