53edcf028b366c0c9c17a76a9ffa32a749e4b489
Jacek Kopecky <jacek@jacek.cz>
2021-01-01 19:49:43 +0000

    fix tests

--- tests/components/testing-app-states.ts
+++ tests/components/testing-app-states.ts
@@ -1,10 +1,33 @@
+import * as React from 'react';
+
 import { AppState } from '../../src/js/AppState';
 
 import * as reactTools from '../../src/js/tools/react';
 
+interface ReactModule {
+  useState: unknown,
+}
+
+jest.mock('react', () => {
+  const actualReact: ReactModule = jest.requireActual('react');
+
+  const setState = jest.fn();
+  const useStateSpy = jest.fn();
+  useStateSpy.mockImplementation(
+    ((initialValue: unknown) => [initialValue, setState]) as
+      () => [unknown, React.Dispatch<unknown>],
+  );
+
+  return {
+    ...actualReact,
+    useState: useStateSpy,
+    originalUseState: actualReact.useState,
+  };
+});
+
 export function empty(): AppState {
-  const spy = jest.spyOn(reactTools, 'useLocalStorage');
-  spy.mockImplementation((key, initialValue) => [initialValue, () => {}, () => {}]);
+  const useLocalStorageSpy = jest.spyOn(reactTools, 'useLocalStorage');
+  useLocalStorageSpy.mockImplementation((key, initialValue) => [initialValue, () => {}, () => {}]);
 
   return new AppState();
 }
