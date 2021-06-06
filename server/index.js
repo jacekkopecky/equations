5e93acfc322d5c04a6441aee08be52494fd38b93
Jacek Kopecky <jacek@jacek.cz>
2021-06-06 20:13:44 +0100

    server: GNU Terry Pratchett

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(cors());
 
+// "A man is not dead while his name is still spoken."
+// - Going Postal, Chapter 4 prologue
+app.use((req, res, next) => {
+  res.set('X-Clacks-Overhead', 'GNU Terry Pratchett');
+  next();
+});
+
 app.get('/', (req, res) => { res.send('function running'); });
 
 app.use('/api', api);
